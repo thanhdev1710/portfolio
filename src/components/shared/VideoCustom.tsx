d2616ad44bc2379189fcac12e4675ad8c59ff3db
chithanh1710 <chithanh171004@gmail.com>
2024-11-30 22:25:09 +0700

    video loading

--- src/components/shared/VideoCustom.tsx
+++ src/components/shared/VideoCustom.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { motion, useInView } from "framer-motion";
 
 export default function VideoCustom({ video }: { video: string }) {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [isLoading, setIsLoading] = useState(true); // State để theo dõi trạng thái tải video
 
   // Sử dụng useInView để theo dõi khi phần tử nằm trong màn hình
   const isInView = useInView(videoRef, {
@@ -21,6 +22,15 @@ export default function VideoCustom({ video }: { video: string }) {
     }
   }
 
+  // Xử lý sự kiện khi video bắt đầu tải và khi video tải xong
+  const handleLoadStart = () => {
+    setIsLoading(true);
+  };
+
+  const handleLoadedData = () => {
+    setIsLoading(false);
+  };
+
   return (
     <motion.div
       className="relative aspect-video w-full h-auto overflow-hidden rounded-2xl"
@@ -30,12 +40,21 @@ export default function VideoCustom({ video }: { video: string }) {
       }
       transition={{ duration: 0.8, ease: "easeInOut" }}
     >
+      {/* Hiển thị spinner khi video đang tải */}
+      {isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="w-8 h-8 border-4 border-t-transparent border-white border-solid rounded-full animate-spin"></div>
+        </div>
+      )}
+
       <video
         ref={videoRef}
         className="w-full h-full object-fill rounded-2xl pointer-events-none"
         muted
         loop
         playsInline
+        onLoadStart={handleLoadStart} // Khi video bắt đầu tải
+        onLoadedData={handleLoadedData} // Khi video tải xong
       >
         <source src={video} type="video/mp4" />
       </video>
